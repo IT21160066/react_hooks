401555f03aae09fad1dcb6e70cffaa1958e00ab9
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-04-07 20:09:46 +0530

    component channel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Mychannel from "./components/MyChannel/Mychannel";
+
 function App() {
-  return <h1 className="heading">React Hooks</h1>;
+  return (
+    <>
+      <h1 className="heading">React Hooks</h1>
+      <Mychannel />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/MyChannel/Mychannel.jsx
@@ -0,0 +1,5 @@
+const Mychannel = () => {
+  return <div>Mychannel</div>;
+};
+
+export default Mychannel;
